c6adac70858c7dd53a5fb65d67f3478fb1796389
arslan <arslanhafiz091@gmail.com>
2024-06-10 17:24:23 +0500

    design completed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
+
+
+
+
